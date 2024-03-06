47132f7f4e0632a2b4245b1bcb0e570b3b0fa94f
Sean <seanreilly123@hotmail.com>
2024-03-06 16:16:13 +1100

    Unstage dev config

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGO_URI: `mongodb+srv://test:admintest@licenseplatechat.lh4ebcj.mongodb.net/LicensePlateChat`,
-    CLOUD_NAME: "aotearoadjs",
-    CLOUDINARY_API_KEY: "228334344331647",
-    CLOUDINARY_API_SECRET: "K6eqgFOfM2j8JG5c9ie0PpGj5j0",
-    JWT_TOKEN:
-        "goK!pusp6ThEdURUtRenOwUhAsWUCLheBazl!uJLPlS8EbreWLdrupIwabRAsiBu",
-};
